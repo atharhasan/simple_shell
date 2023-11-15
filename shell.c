80a38f6d3aeb91aed8287b2536e9f22e788e15cb
atharhasan <athar_h12@yahoo.com>
2023-11-15 03:03:23 +0200

    commit task

--- shell.c
+++ shell.c
@@ -1,24 +1,39 @@
 #include "shell.h"
 
 /**
+ * handle_env - function that handle env
+ * @env2: Another array of strings for "env" command
+ */
+void handle_env(char **env2)
+{
+	while (*env2)
+	{
+		write(STDOUT_FILENO, *env2, _strlen(*env2));
+		write(STDOUT_FILENO, "\n", 1);
+		env2++;
+	}
+}
+
+/**
  * main - shell main file
  * @argc: The number of command-line arguments but it's not used
  * @env: an array of strings
  * @env2: Another array of strings for "env" command
  * Return: value
  */
-
 int main(int argc, char **env, char **env2)
 {
-	char command[512], *token, *args[512];
-	int argCount = 0, i, value = 0, error_count = 0;
+	char *command = NULL, *token, *args[512];
+	int argCount = 0, i, value = 0, error_count = 0, linval;
+	size_t len = 0;
 	(void)argc;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "($) ", 4);
-		if (fgets(command, 512, stdin) == NULL)
+		linval = getline(&command, &len, stdin);
+		if (linval == -1)
 			break;
 		error_count++;
 		command[strcspn(command, "\n")] = '\0';
@@ -35,12 +50,7 @@ int main(int argc, char **env, char **env2)
 				break;
 			if (_strcmp(args[0], "env") == 0)
 			{
-				while (*env2)
-				{
-					write(STDOUT_FILENO, *env2, _strlen(*env2));
-					write(STDOUT_FILENO, "\n", 1);
-					env2++;
-				}
+				handle_env(env2);
 				continue;
 			}
 			value = cd_handle(args, env, argCount, error_count);
@@ -49,5 +59,6 @@ int main(int argc, char **env, char **env2)
 			args[i] = NULL;
 		argCount = 0;
 	}
+	free(command);
 	return (value);
 }
